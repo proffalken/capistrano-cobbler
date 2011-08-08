ecb2865c319cff382a86d7db6e1b64d4cf3f3dab
proffalken <theprofessor@threedrunkensysadsonthe.net>
2011-08-08 20:35:40 +0100

    Slowly adding tests

--- lib/capistranoCobbler.rb
+++ lib/capistranoCobbler.rb
@@ -1,8 +1,27 @@
+# This class enables Capistrano to retrieve the server-list of applicable nodes
+# from Cobbler based upon the Management Class (mgmt_classes) defined for a node
 class CapistranoCobbler
 
-    attr_reader :cblr_server
+    attr_reader :mgmt_class
+    attr_reader :server_list
+    attr_reader :cobbler_server
 
-    def initialize(cobbler_server = '')
-    	@cblr_server = cobbler_server
+    def initialize(mgmt_class,cobbler_server = "localhost")
+        require 'xmlrpc/client'
+        @cobbler_server = cobbler_server
+        @mgmt_class = mgmt_class
+        @connection = XMLRPC::Client.new2("http://#{cobbler_server}/cobbler_api")
+    end
+
+    def get_servers
+        ok, servers = @connection.call2('find_system',:mgmt_classes => @mgmt_class)
+        server_list = Array.new()
+        for server_name in servers do 
+	    ok, fqdn = @connection.call2('get_system_for_koan',server_name)
+            server_list << fqdn['interfaces']['eth0']['hostname']
+        end
+        
+        return server_list
     end
 end
+
--- test/cobbler_connection_test.rb
+++ test/cobbler_connection_test.rb
@@ -3,7 +3,12 @@ require 'capistranoCobbler'
 
 class TestCobblerConnections < Test::Unit::TestCase
     def test_server_url
-    	cobbler_connection = CapistranoCobbler.new('localhost')
-    	assert_equal('localhost', cobbler_connection.cblr_server)
+    	cobbler_connection = CapistranoCobbler.new('www','localhost')
+    	assert_equal('localhost', cobbler_connection.cobbler_server)
+    end
+
+    def test_management_class_is_passed_correctly
+    	cobbler_connection = CapistranoCobbler.new('www','localhost')
+    	assert_equal('www',cobbler_connection.mgmt_class)
     end
 end
